40b15d70e4224a0d5300014ac55b78ed12ca0d4b
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-04-17 11:31:05 +0530

    successfully got quotes from API

---
+++ src/Quotes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import requests
+import json
+
+class Quotes:
+
+    def __init__(self):
+        pass
+    
+    def getQuote(self):
+        url = "https://quote-garden.herokuapp.com/api/v2/quotes/random"
+        response = requests.get(url)
+        data = response.text
+        parsed = json.loads(data)
+        quote = json.dumps(parsed, indent = 4)
+        return quote
---
+++ src/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from Quotes import Quotes
+if __name__ == "__main__":
+    Q = Quotes()
+    print(Q.getQuote())
+    
