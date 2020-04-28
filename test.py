e8769a4ac262c46962765a36f8b807bf2e1362d8
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-04-28 18:02:48 +0530

    test.py

--- test.py
+++ test.py
@@ -12,31 +12,46 @@ class TestAPI(unittest.TestCase):
         
      # Test for URL name
         # Try to add 'abc' url. It should successfully add
-        self.assertEqual(api.addAPI_URL('abc', 'abc.com'), True, 'abc url should be added successfully')
+        self.assertEqual(api.addAPI_URL('abc', 'example.com'), None, 'Function should return nothing on success.\
+        Exception on failure')
         
-        #Null URL name
+        #Null URL name should throw exception
         with self.assertRaises(Exception) as context:
-            api.addAPI_URL('', 'abc.com')
+            api.addAPI_URL('', 'example.com')
         self.assertTrue('null URL name' in context.exception)
         
-        #duplicate URL name
+        #duplicate URL name should throw exception
         with self.assertRaises(Exception) as context:
-            api.addAPI_URL('abc', 'abc.com')
-        self.assertTrue('Duplicate URL name' in context.exception)
+            api.addAPI_URL('abc', 'google.com')
+        self.assertTrue('Duplicate URL name' in context.exception)        
         
     # Test for URL
+        api2 = exoREST()
+        api2.addAPI_URL('abc', 'example.com')
+        
         # check for URL formatting is correct
+        with self.assertRaises(Exception) as context:
+            api2.addAPI_URL('def', 'example/com')
         
         # Raise exception if URL is null
         with self.assertRaises(Exception) as context:
-            api.addAPI_URL('abc', '')
+            api2.addAPI_URL('abc', '')
         self.assertTrue('Null URL' in context.exception)
         
+        # Raise warning if same URL has been added already
+        with self.assertRaises(Warning) as context:
+            api2.addAPI_URL('def', 'example.com')
+        self.assertTrue('duplicate URL' in context.warning)
+
+class TestSocialBot(unittest.TestCase):                
     def test_socialBot(self):
-        self.assertEqual(True, True)
+        pass
+class TestMotivateU(unittest.TestCase):
     def test_MotivateU(self):
-        self.assertEqual(True, True)
+        pass
+class TestMotivateU_GUI(unittest.TestCase):
     def test_gui(self):
-        self.assertEqual(True, True)
+        pass
+        
 if __name__ == '__main__':
     unittest.main()
